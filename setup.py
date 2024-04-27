c05880861d06fb9b64c29476a9bb9b9fd15d2c1b
tk42 <nsplat@gmail.com>
2024-04-27 16:40:55 +0900

    Update dependencies in setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup
 
 setup(
     name="ssm",
-    version="1.1.1",
+    version="1.1.2",
     install_requires=[
         "skorch",
         "pytorch_lightning",
         "einops",
         "jax",
+        "flax",
+        "equinox",
     ],
 )
